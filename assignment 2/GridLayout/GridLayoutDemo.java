01d74fcf094fe843ab89c61cd2f5f6525a2650b0
shreejan9860 <shreezanpandit@gmail.com>
2020-03-02 09:53:30 -0500

    assignment 2 gridlayout

---
+++ assignment 2/GridLayout/GridLayoutDemo.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import javax.swing.*;
+
+class GridLayoutDemo
+{
+	GridLayoutDemo()
+	{
+		super();
+		JFrame f = new JFrame();
+
+		JButton btn1 = new JButton("1");
+		JButton btn2 = new JButton("2");
+		JButton btn3 = new JButton("3");
+		JButton btn4 = new JButton("4");
+		JButton btn5 = new JButton("5");
+		JButton btn6 = new JButton("6");
+		JButton btn7 = new JButton("7");
+		JButton btn8 = new JButton("8");
+		JButton btn9 = new JButton("9");
+
+		f.add(btn1);
+		f.add(btn2);
+		f.add(btn3);
+		f.add(btn4);
+		f.add(btn5);
+		f.add(btn6);
+		f.add(btn7);
+		f.add(btn8);
+		f.add(btn9);
+
+		f.setLayout(new GridLayout(3,3));
+		f.setSize(300,300);
+		f.setVisible(true);
+	}
+
+	public static void main(String[] args) 
+	{
+		new GridLayoutDemo();
+	}
+}
